65d81786660d348a893d62c108e61b7ec2475928
mili <milimansilla444@gmail.com>
2023-12-04 22:37:52 -0300

    create order

--- FrontEnd/src/componentes/orden/create_order.js
+++ FrontEnd/src/componentes/orden/create_order.js
@@ -1,83 +1,70 @@
-import React, { useContext, useEffect, useState} from "react";
-
-import Cookies from "universal-cookie";
-import swal from "sweetalert2";
-const Cookie = new Cookies();
-const id_user = setUser()
-
-function setUser (){
-  let cookieUser = Cookie.get("username")
-
-  if(cookieUser!=undefined){
-  let array = cookieUser.split(",")
-  return array[0]
-  }else{
-    return "undefined"
-  }
-}
-function vaciarCarrito(){
-   
-    document.cookie = "cart"+id_user+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-
-async function GetProductById(id){
-  return fetch("http://localhost:8090/product/" + id).then(response => response.json())
-}
-export const CrearOrden = ()=>{
-  
-
-    let cookie = Cookie.get("username")
+export const CrearOrden = async () => {
+  try {
+    let cookie = Cookie.get("username");
     let id_user = parseInt(cookie.split(",")[0]);
-    let orderDetail ={'product_id':0,'quantity':0}
+    let orderDetail = { 'product_id': 0, 'quantity': 0, 'unit_price': 0 };
     let ordersDetail = [];
 
-    let a = Cookie.get("cart"+id_user).split(";")
- 
+    let a = Cookie.get("cart" + id_user).split(";");
 
-    for (let i = 0; i < a.length; i++){
+    for (let i = 0; i < a.length; i++) {
       let item = a[i];
-      if(item != ""){
-        let array = item.split(",")
-        orderDetail ={'product_id':0,'quantity':0}
-        orderDetail.product_id = parseInt(array[0])
-        orderDetail.quantity =parseInt(array[1])
-         
-        ordersDetail.push(orderDetail)
-        
+      if (item != "") {
+        let array = item.split(",");
+        let product_id = parseInt(array[0]);
+        let quantity = parseInt(array[1]);
+
+        // Obtener información del producto, incluyendo el precio unitario
+        let product = await GetProductById(product_id);
+        let unit_price = product.product_unit_price;
+
+        orderDetail = {
+          'product_id': product_id,
+          'quantity': quantity,
+          'unit_price': unit_price
+        };
+
+        ordersDetail.push(orderDetail);
       }
     }
-    
-    
-    const crearOrden = async()=>{
-      fetch('http://localhost:8090/neworder',requestOptions)
-      .then(response => {if (response.status != 201) {
-        swal.fire({icon: 'error', text:"No se pudo realizar la compra"}
-        ).then((result) => {
-          if (result.isConfirmed) {
-            window.location.reload();
-            return response.json()
-          }}) 
-       }else{
-          swal.fire({icon: 'success', text:"Compra realizada con éxito"}
-          ).then((result) => {
-            if (result.isConfirmed) {
-              window.location.replace("/inicio")
-              vaciarCarrito()
-              return response.json()
-            }})
-         
-        }})
-      };
-      const requestOptions={
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        
-        body: JSON.stringify({
-             user_id: id_user,
-             details: ordersDetail
-    })
-    };
-     
-    crearOrden();
 
-}
+    const crearOrden = async () => {
+      fetch('http://localhost:8090/neworder', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          user_id: id_user,
+          details: ordersDetail
+        })
+      })
+        .then(response => {
+          if (response.status !== 201) {
+            swal.fire({
+              icon: 'error',
+              text: "No se pudo realizar la compra"
+            }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.reload();
+                return response.json();
+              }
+            });
+          } else {
+            swal.fire({
+              icon: 'success',
+              text: "Compra realizada con éxito"
+            }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.replace("/inicio");
+                vaciarCarrito();
+                return response.json();
+              }
+            });
+          }
+        });
+    };
+
+    crearOrden();
+  } catch (error) {
+    console.error("Error al crear la orden:", error);
+  }
+};
